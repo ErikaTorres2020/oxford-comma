5b12d9482447aafc75d0441a470dbbdcdc6853e2
Erika Torres <+>
2021-03-01 14:31:34 -0500

    commit tree c554a64af43245d729e566f88c40e833f8ae07f0

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"  
+  elsif array.length == 1
+    return "#{array[0]}"  
+  else 
+    array[-1].insert(0,"and ")
+
+  end
+  array.join(", ")
 end
